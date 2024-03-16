cea54ad94f72aa013e49127a92f3ecbb4315a93a
crisrm08 <148398811+crisrm08@users.noreply.github.com>
2024-03-16 18:50:21 -0400

    Update game.js

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,7 +1,6 @@
 var gamePattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
 
-$("h1").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
 
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 3) + 1;
