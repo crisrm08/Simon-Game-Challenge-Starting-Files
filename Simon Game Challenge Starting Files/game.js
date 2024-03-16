81ff1ff012365d47bfc6bdc57c4575af98ece2d5
Cristian <crisgues8@gmail.com>
2024-03-16 18:30:52 -0400

    simon

---
+++ Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,17 @@
+var gamePattern = [];
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 3) + 1;
+    console.log(randomNumber);
+}
+
+var randomChosenColour = buttonColours[randomNumber];
+gamePattern.push(randomChosenColour);
+
+$("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+$("h1").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+$("button").addClass("pressed");
+
